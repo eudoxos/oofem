863731ebba3b8357c4006090701518f66ca7fab8
Václav Šmilauer <eu@doxos.eu>
2025-12-12 23:17:44 +0100

    metastep: read in way which is also compatible with XML input

--- src/core/timestepcontroller.C
+++ src/core/timestepcontroller.C
@@ -53,8 +53,7 @@ void
 TimeStepController :: initializeFrom(InputRecord &ir)
 {
 
-  numberOfMetaSteps   = 0;
-  IR_GIVE_OPTIONAL_FIELD(ir, numberOfMetaSteps, _IFT_TimeStepController_nmsteps);
+  numberOfMetaSteps = ir.giveReader()->giveGroupRecords(ir.ptr(),_IFT_EngngModel_nmsteps,"Metasteps",DataReader::IR_mstepRec,/*optional*/true).size();
   IR_GIVE_OPTIONAL_FIELD(ir, this->alpha, _IFT_TimeStepController_alpha);
 
 }
@@ -102,11 +101,10 @@ TimeStepController :: giveNextStep()
     return currentStep.get();
 }
 
-
 int
 TimeStepController :: instanciateMetaSteps(DataReader &dr)
 {
-    double totalNumberOfSteps;
+    [[maybe_unused]] double totalNumberOfSteps=0.;
     // create meta steps
     metaStepList.clear();
     metaStepList.reserve(this->numberOfMetaSteps);
@@ -114,19 +112,19 @@ TimeStepController :: instanciateMetaSteps(DataReader &dr)
         //MetaStep *mstep = new MetaStep(i, this);
         metaStepList.emplace_back(i, eModel);
     }
-
     // read problem domains
-    for ( int i = 1; i <= this->numberOfMetaSteps; i++ ) {
-        auto &ir = dr.giveInputRecord(DataReader :: IR_mstepRec, i);
-        metaStepList[i-1].initializeFrom(ir);
-	totalNumberOfSteps += metaStepList[i-1].giveNumberOfSteps();
+    auto mrecs=dr.giveGroupRecords("Metasteps",DataReader::IR_mstepRec,this->numberOfMetaSteps);
+    int i=0;
+    for(InputRecord& mrec: mrecs){
+        metaStepList[i].initializeFrom(mrec);
+        totalNumberOfSteps += metaStepList[i].giveNumberOfSteps();
+        i++;
     }
 
     //this->numberOfSteps = metaStepList.size();
     //this->recomputeFinalTime(totalNumberOfSteps);
     return 1;
 }
-
 
 MetaStep *
 TimeStepController :: giveMetaStep(int i)
